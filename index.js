807517bc1a9fe15a095edfee298cee0c4cab77e3
Mollakhaled <khaledbalok@gmail.com>
2024-07-11 21:49:24 +0600

    the-daily-newspaper-server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
+app.use(cors());
+
+app.get('/categories', (req, res) => {
+  res.send(categories)
+})
+// all news
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+// single news or specific news
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+})
+
+// specific category news
+app.get('/categories/:id',(req, res) =>{
+  const id = parseInt (req.params.id);
+  if(id === 0){
+    res.send(news)
+  }
+  else{
+    const categoryNews = news.filter(n=> parseInt(n.category_id) === id)
+     res.send(categoryNews);
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
